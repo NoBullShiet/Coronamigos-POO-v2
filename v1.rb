17429dfe5b1cd37249ad759b99c9a87d0fca5327
andresinope77 <63989998+andresinope77@users.noreply.github.com>
2020-04-25 20:47:05 -0500

    Update v1.rb

--- v1.rb
+++ v1.rb
@@ -5,28 +5,80 @@
 class Alumno
 	attr_accessor :dni, :nombre, :apellido, :edad, :genero
 	def initialize(dni, nombre, apellido, edad, genero)
-  		@dni, @nombre, @apellido, @edad, @genero = dni, nombre, apellido, edad, genero
-  	end
+  	@dni, @nombre, @apellido, @edad, @genero = dni, nombre, apellido, edad, genero
+  end
 end
 
 class AlumnoNacional < Alumno
 	attr_accessor :tipo, :promedio2do
-  	def initialize(dni, nombre, apellido, edad, genero, tipo, promedio2do)
-  		super(dni, nombre, apellido, edad, genero)
-  		@colegio = "NACIONAL"
-  		@tipo = tipo
-  		@promedio2do = promedio2do
-  	end
+  def initialize(dni, nombre, apellido, edad, genero, tipo, promedio2do)
+ 		super(dni, nombre, apellido, edad, genero)
+ 		@colegio = "NACIONAL"
+ 		@tipo = tipo
+ 		@promedio2do = promedio2do
+ 	end
+
+ 	def calificarSocioEconomica
+ 		if tipo == "RURAL"
+ 			return 100
+		elsif tipo == "URBANA"
+ 			return 100
+ 		else
+ 			return 0
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if promedio2do < 11
+ 			return 0
+		elsif promedio2do < 14
+ 			return 20
+		elsif promedio2do < 16
+ 			return 40
+		elsif promedio2do < 18
+ 			return 60
+		elsif promedio2do < 19
+ 			return 80
+ 		else
+ 			return 100
+ 		end
+ 	end
 end
 
 class AlumnoParticular < Alumno
 	attr_accessor :pension, :puesto2do
-  	def initialize(dni, nombre, apellido, edad, genero, pension, puesto2do)
-  		super(dni, nombre, apellido, edad, genero)
-  		@colegio = "PARTICULAR"
-  		@pension = pension
-  		@puesto2do = puesto2do
-  	end
+ 	def initialize(dni, nombre, apellido, edad, genero, pension, puesto2do)
+ 		super(dni, nombre, apellido, edad, genero)
+ 		@colegio = "PARTICULAR"
+ 		@pension = pension
+ 		@puesto2do = puesto2do
+ 	end
+
+ 	def calificarSocioEconomica
+ 		if pension <= 200
+ 			return 90
+  	elsif pension <= 400
+  		return 70
+		elsif pension <= 600
+ 			return 50
+ 		else
+ 			return 40
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if puesto2do < 4
+ 			return 100
+		elsif puesto2do < 6
+ 			return 80
+		elsif puesto2do < 11
+ 			return 60
+		elsif puesto2do < 21
+ 			return 40
+ 		else
+ 			return 0
+ 		end
+ 	end
 end
 
 class Tutor
@@ -48,3 +100,4 @@ class Ministerio
 		@listaAlumnos = Array.new
 	end
 end
+
