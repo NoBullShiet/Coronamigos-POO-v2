4159896ba77984e005b37cff68ba9ed1e69eebd9
NoBullShiet <64295997+NoBullShiet@users.noreply.github.com>
2020-04-28 19:43:54 -0500

    Update Main Code

--- Main Code.rb
+++ Main Code.rb
@@ -16,6 +16,32 @@ class AlumnoNacional < Alumno
  		@tipo = tipo.upcase
  		@promedio2do = promedio2do
  	end
+	
+	def calificarSocioEconomica
+ 		if tipo == "RURAL"
+ 			return 100
+		elsif tipo == "URBANA"
+ 			return 100
+ 		else
+ 			return 0
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if promedio2do < 11
+ 			return 0
+		elsif promedio2do < 14
+ 			return 20
+		elsif promedio2do < 16
+ 			return 40
+		elsif promedio2do < 18
+ 			return 60
+		elsif promedio2do < 19
+ 			return 80
+ 		else
+ 			return 100
+ 		end
+ 	end
 end
 
 class AlumnoParticular < Alumno
@@ -26,4 +52,47 @@ class AlumnoParticular < Alumno
  		@pension = pension
  		@puesto2do = puesto2do
  	end
+	
+	def calificarSocioEconomica
+ 		if pension <= 200
+ 			return 90
+  	elsif pension <= 400
+  		return 70
+		elsif pension <= 600
+ 			return 50
+ 		else
+ 			return 40
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if puesto2do < 4
+ 			return 100
+		elsif puesto2do < 6
+ 			return 80
+		elsif puesto2do < 11
+ 			return 60
+		elsif puesto2do < 21
+ 			return 40
+ 		else
+ 			return 0
+ 		end
+ 	end
+end
+
+class Tutor
+	attr_accessor :dniAlumno, :dniTutor, :nombre, :apellido, :parentesco
+	def initialize(dniAlumno, dniTutor, nombre, apellido, parentesco)
+		@dniAlumno, @dniTutor, @nombre, @apellido, @parentesco = dniAlumno, dniTutor, nombre, apellido, parentesco
+	end
+end
+
+class Examen
+	attr_accessor :codigoEvaluacion, :numeroPregunta, :listaRespuestasAlumno, :listaRespuestasCorrectas
+	def initialize(codigoEvaluacion, numeroPregunta)
+		@codigoEvaluacion, @numeroPregunta = codigoEvaluacion, numeroPregunta
+    @listaRespuestasAlumno = Array.new(numeroPregunta)
+    @listaRespuestasCorrectas = Array.new(numeroPregunta)
+	end
+	
 end
